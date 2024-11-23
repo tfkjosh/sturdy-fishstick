6a6de3412f0ea733b2b2499c76a089be27fb9063
Josh K <jkg050604@gmail.com>
2024-11-23 11:00:13 -0600

    start localhost

--- src/app/components/layout/navbar/index.tsx
+++ src/app/components/layout/navbar/index.tsx
@@ -2,6 +2,7 @@ import { getMenu } from "@/app/lib/shopify";
 
 export async function Navbar() {
     const menu = await getMenu("nextjs-frontend-menu");
+
     console.log(menu);
-    return ;
+    return <nav> </nav>;
 }
