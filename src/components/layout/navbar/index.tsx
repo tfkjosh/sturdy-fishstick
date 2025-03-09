a4f2a7fe4674e166909ad9623fcdfca3325890e4
joshua-king <140848241+tfkjosh@users.noreply.github.com>
2025-03-09 15:06:41 -0500

    border logo

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -33,7 +33,7 @@ export async function Navbar() {
                   <Link
                     href={item.path}
                     prefetch={true}
-                    className="text-gray-700 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                    className="text-gray-950 underline-offset-4 hover:text-black font-normal hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
                   >
                     {item.title}
                   </Link>
