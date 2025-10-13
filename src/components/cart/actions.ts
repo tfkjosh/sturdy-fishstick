f310c375548acd19c0deee68d11da088adc2fc8b
joshua-king <140848241+tfkjosh@users.noreply.github.com>
2025-10-13 00:31:57 -0400

    shopify checkout

--- src/components/cart/actions.ts
+++ src/components/cart/actions.ts
@@ -9,14 +9,14 @@ import {
   updateCart,
 } from "@/lib/shopify";
 import { revalidateTag } from "next/cache";
-import { cookies } from "next/headers";
+import { cookies } from "next/headers"; 
 import { redirect } from "next/navigation";
 
 export async function addItem(
   prevState: any,
   selectedVariantId: string | undefined
 ) {
-  let cartId = (await cookies()).get("cartId")?.value;
+  let cartId = cookies().get("cartId")?.value;
 
   if (!cartId || !selectedVariantId) {
     return "Error adding item to cart";
@@ -39,7 +39,7 @@ export async function updateItemQuantity(
     quantity: number;
   }
 ) {
-  let cartId = (await cookies()).get("cartId")?.value;
+  let cartId = cookies().get("cartId")?.value;
   if (!cartId) {
     return "Missing cart ID";
   }
@@ -81,7 +81,7 @@ export async function updateItemQuantity(
 }
 
 export async function removeItem(prevState: any, merchandiseId: string) {
-  let cartId = (await cookies()).get("cartId")?.value;
+  let cartId = cookies().get("cartId")?.value;
 
   if (!cartId) {
     return "Missing cart ID";
@@ -109,7 +109,7 @@ export async function removeItem(prevState: any, merchandiseId: string) {
 }
 
 export async function redirectToCheckout() {
-  let cartId = (await cookies()).get("cartId")?.value;
+  let cartId = cookies().get("cartId")?.value;
 
   if (!cartId) {
     return "Missing cart ID";
@@ -126,5 +126,5 @@ export async function redirectToCheckout() {
 
 export async function createCartAndSetCookie() {
   let cart = await createCart();
-  (await cookies()).set("cartId", cart.id!);
+  cookies().set("cartId", cart.id!);
 }
